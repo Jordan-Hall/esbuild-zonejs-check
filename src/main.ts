1a6cdc0ea6b52e25bd4b2795bda0c07845c2c734
e-oz <normandiggs@gmail.com>
2023-11-29 08:43:27 +0100

    upd

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import 'zone.js';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
@@ -16,6 +15,19 @@ import { bootstrapApplication } from '@angular/platform-browser';
 })
 export class App {
   name = 'Angular';
+  constructor() {
+
+    const zone = Zone.current.fork({name: 'myZone'});
+    zone.run(() => {
+      requestAnimationFrame(() => {
+        console.log('requestAnimationFrame() callback is invoked in the zone', Zone.current.name);
+        // since the callback of `requestAnimationFrame()` will be in the same zone
+        // when it is scheduled, so the output will be 'requestAnimationFrame() callback is invoked
+        // in the zone myZone'
+      });
+    });
+
+  }
 }
 
 bootstrapApplication(App);
